b4847cfd6610b0780d1b2696b8f58dc8a90fd964
Lukas Straumann <lukas_straumann@gmx.ch>
2019-06-12 10:09:24 +0200

    Layout stretch fix and logo link fix

--- minerals_app/templatetags/mineral_extras.py
+++ minerals_app/templatetags/mineral_extras.py
@@ -6,8 +6,7 @@ from minerals_app.models import Minerals
 register = template.Library()
 
 
-@register.filter('markdown_to_html')
-def markdown_to_html(markdown_text):
-    """Converts markdown text to html"""
-    html_body = markdown2.markdown(markdown_text)
-    return html_body
+@register.filter('replace_underline')
+def replace_underline(text):
+    """Replaces underline with a space"""
+    return text.replace('_', ' ')
